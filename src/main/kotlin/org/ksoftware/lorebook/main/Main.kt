b73b93d5956e59dfd27d4f85a6310329f900f81d
SKeeneCode <samkeene@protonmail.com>
2020-10-16 11:45:05 +0100

    class header comments

--- src/main/kotlin/org/ksoftware/lorebook/main/Main.kt
+++ src/main/kotlin/org/ksoftware/lorebook/main/Main.kt
@@ -3,6 +3,9 @@ package org.ksoftware.lorebook.main
 import org.ksoftware.lorebook.Styles
 import tornadofx.*
 
+/**
+ * Entry class for the application
+ */
 class LorebookApp : App(MainWorkspace::class, Styles::class)
 
 fun main() {
--- src/main/kotlin/org/ksoftware/lorebook/main/MainWorkspace.kt
+++ src/main/kotlin/org/ksoftware/lorebook/main/MainWorkspace.kt
@@ -2,6 +2,9 @@ package org.ksoftware.lorebook.main
 
 import tornadofx.*
 
+/**
+ * Workspace implementation used for the applications main window.
+ */
 class MainWorkspace : Workspace("Lorebook", NavigationMode.Tabs) {
     init {
         primaryStage.width = 1200.0
