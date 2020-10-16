ac825751462713bca731d7b1ea48d457e5d2ec50
SKeeneCode <samkeene@protonmail.com>
2020-10-16 11:33:34 +0100

    kotlin 1.4.1 and javafx 15

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "org.example"
+group = "org.ksoftware"
 version = "1.0-SNAPSHOT"
 
 plugins {
     application
     java
-    kotlin("jvm") version "1.4.0-rc"
-    id("org.openjfx.javafxplugin") version "0.0.8"
+    kotlin("jvm") version "1.4.10"
+    id("org.openjfx.javafxplugin") version "0.0.9"
 }
 
 application {
-    mainClassName = "org.example.MyAppKt"
+    mainClassName = "org.ksoftware.lorebook.MyAppKt"
 }
 kotlin {
     tasks.withType<KotlinCompile> {
@@ -22,7 +22,7 @@ kotlin {
 }
 
 javafx {
-    version = "14"
+    version = "15"
     modules = mutableListOf(
             "javafx.controls",
             "javafx.graphics",
@@ -39,7 +39,7 @@ repositories {
 dependencies {
     implementation ("no.tornado:tornadofx:2.0.0-SNAPSHOT")
     implementation("org.fxmisc.richtext:richtextfx:0.10.5")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
     testImplementation("org.testfx:testfx-core:4.0.16-alpha")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
     testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
