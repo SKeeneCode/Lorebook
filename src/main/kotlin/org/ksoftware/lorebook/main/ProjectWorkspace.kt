a14d3e7fd8ae883b90ee4824c045595691e007fc
SKeeneCode <samkeene@protonmail.com>
2021-02-16 16:09:44 +0000

    move richtext toolbar to own class

--- src/main/kotlin/org/ksoftware/lorebook/main/ProjectWorkspace.kt
+++ src/main/kotlin/org/ksoftware/lorebook/main/ProjectWorkspace.kt
@@ -1,22 +1,13 @@
 package org.ksoftware.lorebook.main
 
 import ch.micheljung.fxwindow.FxStage
-import ch.micheljung.fxwindow.WindowController
 import de.jensd.fx.glyphs.materialicons.MaterialIcon
 import de.jensd.fx.glyphs.materialicons.MaterialIconView
-import javafx.geometry.Orientation
-import javafx.geometry.Pos
 import javafx.scene.control.MenuBar
-import javafx.scene.control.ToggleGroup
-import javafx.scene.input.TransferMode
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
-import javafx.scene.text.Font
-import org.controlsfx.dialog.FontSelectorDialog
-import org.ksoftware.lorebook.controls.TextSizePicker
 import org.ksoftware.lorebook.newproject.NewProjectView
-import org.ksoftware.lorebook.nodes.TextController
-import org.ksoftware.lorebook.richtext.TextAlignment
+import org.ksoftware.lorebook.richtext.RichTextToolbarView
 import org.ksoftware.lorebook.richtext.ToolbarViewModal
 import org.ksoftware.lorebook.styles.Styles
 import tornadofx.*
@@ -30,7 +21,7 @@ class ProjectWorkspace : Workspace("Lorebook", NavigationMode.Tabs) {
     private val projectController: ProjectWorkspaceController by inject()
     private val projectViewModel: ProjectViewModel by inject()
     private val toolbarViewModal: ToolbarViewModal by inject()
-    private val textController: TextController by inject(FX.defaultScope)
+
     private val overlay = Pane().apply {
         hgrow = Priority.ALWAYS
         vgrow = Priority.ALWAYS
@@ -90,210 +81,19 @@ class ProjectWorkspace : Workspace("Lorebook", NavigationMode.Tabs) {
     init {
         with(header) {
             style {
+                background = null
                 spacing = 0.px
             }
-            hbox(2.0) {
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.HOME).apply {
-                        glyphSize = 24
-                    }
-                    action { }
+            button {
+                addClass(Styles.hoverPopup)
+                addClass(Styles.toolbarButton)
+                background = null
+                graphic = MaterialIconView(MaterialIcon.HOME).apply {
+                    glyphSize = 24
                 }
-                separator(Orientation.VERTICAL)
+                action { }
             }
-            hbox(2.0) {
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.NOTE_ADD).apply {
-                        glyphSize = 26
-                        fill = Color.DARKGREEN
-                    }
-                    action { }
-                }
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.ADD_BOX).apply {
-                        glyphSize = 26
-                        fill = Color.DARKGREEN
-                    }
-                    action { }
-                }
-                separator(Orientation.VERTICAL)
-            }
-            hbox(2.0) {
-                val fontSelector =
-                    FontSelectorDialog(Font.font(toolbarViewModal.fontFamily.value.orElse(Font.getDefault().family)))
-                label {
-                    style {
-                        backgroundColor += Color.WHITE
-                    }
-                    alignment = Pos.CENTER
-                    minWidth = Region.USE_PREF_SIZE
-                    prefWidth = 120.0
-                    maxWidth = Region.USE_PREF_SIZE
-                    minHeight = Region.USE_PREF_SIZE
-                    prefHeight = 32.0
-                    maxHeight = Region.USE_PREF_SIZE
-
-                    onLeftClick {
-                        val font = fontSelector.showAndWait()
-                        if (font.isPresent) {
-                            toolbarViewModal.fontSize.value = Optional.of(font.get().size)
-                            toolbarViewModal.fontName.value = Optional.of(font.get().name)
-                            toolbarViewModal.fontFamily.value = Optional.of(font.get().family)
-                            when (font.get().style) {
-                                "Bold" -> toolbarViewModal.bold.value = Optional.of(true)
-                                "Italic" -> toolbarViewModal.italic.value = Optional.of(true)
-                                "Bold Italic" -> {
-                                    toolbarViewModal.bold.value = Optional.of(true)
-                                    toolbarViewModal.italic.value = Optional.of(true)
-                                }
-                                "Regular" -> {
-                                    toolbarViewModal.bold.value = Optional.of(false)
-                                    toolbarViewModal.italic.value = Optional.of(false)
-                                }
-                            }
-                            toolbarViewModal.triggerTextChange()
-                            toolbarViewModal.updateFontSelectionText(this)
-                        }
-                    }
-
-                    toolbarViewModal.updateLabelTrigger.onChange {
-                        toolbarViewModal.updateFontSelectionText(this)
-                    }
-
-                }
-                add<TextSizePicker>()
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    selectedProperty().onChange { toolbarViewModal.bold.value = Optional.of(it) }
-                    toolbarViewModal.bold.onChange { if (it != null) isSelected = it.orElse(false) }
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_BOLD).apply { glyphSize = 24 }
-                    action {
-                        toolbarViewModal.triggerTextChange()
-                    }
-                }
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    selectedProperty().onChange { toolbarViewModal.italic.value = Optional.of(it) }
-                    toolbarViewModal.italic.onChange { if (it != null) isSelected = it.orElse(false) }
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_ITALIC).apply { glyphSize = 24 }
-                    action {
-                        toolbarViewModal.triggerTextChange()
-                    }
-                }
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_UNDERLINED).apply { glyphSize = 24 }
-                    action { }
-                }
-                togglebutton(ToggleGroup()) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_STRIKETHROUGH).apply { glyphSize = 24 }
-                    action { }
-                }
-                separator(Orientation.VERTICAL)
-            }
-
-            hbox(2.0) {
-                val alignmentToggleGroup = ToggleGroup()
-                togglebutton(alignmentToggleGroup) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_LEFT).apply { glyphSize = 24 }
-                    toolbarViewModal.alignment.onChange { if (it == TextAlignment.LEFT) isSelected = true }
-                    action {
-                        toolbarViewModal.alignment.value = TextAlignment.LEFT
-                        toolbarViewModal.triggerParagraphChange()
-                    }
-                }
-                togglebutton(alignmentToggleGroup) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_CENTER).apply { glyphSize = 24 }
-                    toolbarViewModal.alignment.onChange { if (it == TextAlignment.CENTER) isSelected = true }
-                    action {
-                        toolbarViewModal.alignment.value = TextAlignment.CENTER
-                        toolbarViewModal.triggerParagraphChange()
-                    }
-                }
-                togglebutton(alignmentToggleGroup) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_RIGHT).apply { glyphSize = 24 }
-                    toolbarViewModal.alignment.onChange { if (it == TextAlignment.RIGHT) isSelected = true }
-                    action {
-                        toolbarViewModal.alignment.value = TextAlignment.RIGHT
-                        toolbarViewModal.triggerParagraphChange()
-                    }
-                }
-                togglebutton(alignmentToggleGroup) {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    isSelected = false
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_JUSTIFY).apply { glyphSize = 24 }
-                    toolbarViewModal.alignment.onChange { if (it == TextAlignment.JUSTIFY) isSelected = true }
-                    action {
-                        toolbarViewModal.alignment.value = TextAlignment.JUSTIFY
-                        toolbarViewModal.triggerParagraphChange()
-                    }
-                }
-                separator(Orientation.VERTICAL)
-            }
-            hbox(2.0) {
-                button {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_INDENT_INCREASE).apply { glyphSize = 24 }
-                    action {
-                        toolbarViewModal.triggerIndentIncrease()
-                    }
-                }
-                button {
-                    addClass(Styles.hoverPopup)
-                    addClass(Styles.toolbarButton)
-                    background = null
-                    graphic = MaterialIconView(MaterialIcon.FORMAT_INDENT_DECREASE).apply { glyphSize = 24 }
-                    action {
-                        toolbarViewModal.triggerIndentDecrease()
-                    }
-                }
-                separator(Orientation.VERTICAL)
-            }
-            hbox(2.0) {
-                label(projectViewModel.taskMessage)
-            }
-            background = null
+            add<RichTextToolbarView>()
         }
     }
 
---
+++ src/main/kotlin/org/ksoftware/lorebook/richtext/RichTextToolbarView.kt
@@ -0,0 +1,208 @@
+package org.ksoftware.lorebook.richtext
+
+import de.jensd.fx.glyphs.materialicons.MaterialIcon
+import de.jensd.fx.glyphs.materialicons.MaterialIconView
+import javafx.geometry.Orientation
+import javafx.geometry.Pos
+import javafx.scene.control.ToggleGroup
+import javafx.scene.layout.Region
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import org.controlsfx.dialog.FontSelectorDialog
+import org.ksoftware.lorebook.controls.TextSizePicker
+import org.ksoftware.lorebook.main.ProjectViewModel
+import org.ksoftware.lorebook.styles.Styles
+import tornadofx.*
+import java.util.*
+
+/**
+ * Contains a range of rich text controls such as bold, italic buttons. Communicates with the text area via a
+ * ToolbarViewModal.
+ */
+class RichTextToolbarView : View() {
+
+    private val projectViewModel: ProjectViewModel by inject()
+    private val toolbarViewModal: ToolbarViewModal by inject()
+
+    override val root = hbox {
+        val fontSelector = FontSelectorDialog(Font.font(toolbarViewModal.fontFamily.value.orElse(Font.getDefault().family)))
+        val alignmentToggleGroup = ToggleGroup()
+        separator(Orientation.VERTICAL)
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.NOTE_ADD).apply {
+                glyphSize = 26
+                fill = Color.DARKGREEN
+            }
+            action { }
+        }
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.ADD_BOX).apply {
+                glyphSize = 26
+                fill = Color.DARKGREEN
+            }
+            action { }
+        }
+        separator(Orientation.VERTICAL)
+        label {
+            style {
+                backgroundColor += Color.WHITE
+            }
+            alignment = Pos.CENTER
+            minWidth = Region.USE_PREF_SIZE
+            prefWidth = 120.0
+            maxWidth = Region.USE_PREF_SIZE
+            minHeight = Region.USE_PREF_SIZE
+            prefHeight = 32.0
+            maxHeight = Region.USE_PREF_SIZE
+
+            onLeftClick {
+                val font = fontSelector.showAndWait()
+                if (font.isPresent) {
+                    toolbarViewModal.fontSize.value = Optional.of(font.get().size)
+                    toolbarViewModal.fontName.value = Optional.of(font.get().name)
+                    toolbarViewModal.fontFamily.value = Optional.of(font.get().family)
+                    when (font.get().style) {
+                        "Bold" -> toolbarViewModal.bold.value = Optional.of(true)
+                        "Italic" -> toolbarViewModal.italic.value = Optional.of(true)
+                        "Bold Italic" -> {
+                            toolbarViewModal.bold.value = Optional.of(true)
+                            toolbarViewModal.italic.value = Optional.of(true)
+                        }
+                        "Regular" -> {
+                            toolbarViewModal.bold.value = Optional.of(false)
+                            toolbarViewModal.italic.value = Optional.of(false)
+                        }
+                    }
+                    toolbarViewModal.triggerTextChange()
+                    toolbarViewModal.updateFontSelectionText(this)
+                }
+            }
+
+            toolbarViewModal.updateLabelTrigger.onChange {
+                toolbarViewModal.updateFontSelectionText(this)
+            }
+
+
+        }
+        add<TextSizePicker>()
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            selectedProperty().onChange { toolbarViewModal.bold.value = Optional.of(it) }
+            toolbarViewModal.bold.onChange { if (it != null) isSelected = it.orElse(false) }
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_BOLD).apply { glyphSize = 24 }
+            action {
+                toolbarViewModal.triggerTextChange()
+            }
+        }
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            selectedProperty().onChange { toolbarViewModal.italic.value = Optional.of(it) }
+            toolbarViewModal.italic.onChange { if (it != null) isSelected = it.orElse(false) }
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_ITALIC).apply { glyphSize = 24 }
+            action {
+                toolbarViewModal.triggerTextChange()
+            }
+        }
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_UNDERLINED).apply { glyphSize = 24 }
+            action { }
+        }
+        togglebutton(ToggleGroup()) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_STRIKETHROUGH).apply { glyphSize = 24 }
+            action { }
+        }
+        separator(Orientation.VERTICAL)
+        togglebutton(alignmentToggleGroup) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_LEFT).apply { glyphSize = 24 }
+            toolbarViewModal.alignment.onChange { if (it == TextAlignment.LEFT) isSelected = true }
+            action {
+                toolbarViewModal.alignment.value = TextAlignment.LEFT
+                toolbarViewModal.triggerParagraphChange()
+            }
+        }
+        togglebutton(alignmentToggleGroup) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_CENTER).apply { glyphSize = 24 }
+            toolbarViewModal.alignment.onChange { if (it == TextAlignment.CENTER) isSelected = true }
+            action {
+                toolbarViewModal.alignment.value = TextAlignment.CENTER
+                toolbarViewModal.triggerParagraphChange()
+            }
+        }
+        togglebutton(alignmentToggleGroup) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_RIGHT).apply { glyphSize = 24 }
+            toolbarViewModal.alignment.onChange { if (it == TextAlignment.RIGHT) isSelected = true }
+            action {
+                toolbarViewModal.alignment.value = TextAlignment.RIGHT
+                toolbarViewModal.triggerParagraphChange()
+            }
+        }
+        togglebutton(alignmentToggleGroup) {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            isSelected = false
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_ALIGN_JUSTIFY).apply { glyphSize = 24 }
+            toolbarViewModal.alignment.onChange { if (it == TextAlignment.JUSTIFY) isSelected = true }
+            action {
+                toolbarViewModal.alignment.value = TextAlignment.JUSTIFY
+                toolbarViewModal.triggerParagraphChange()
+            }
+        }
+        separator(Orientation.VERTICAL)
+        button {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_INDENT_INCREASE).apply { glyphSize = 24 }
+            action {
+                toolbarViewModal.triggerIndentIncrease()
+            }
+        }
+        button {
+            addClass(Styles.hoverPopup)
+            addClass(Styles.toolbarButton)
+            background = null
+            graphic = MaterialIconView(MaterialIcon.FORMAT_INDENT_DECREASE).apply { glyphSize = 24 }
+            action {
+                toolbarViewModal.triggerIndentDecrease()
+            }
+        }
+        separator(Orientation.VERTICAL)
+        label(projectViewModel.taskMessage)
+    }
+
+}
